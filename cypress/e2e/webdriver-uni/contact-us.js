0b4cbe66cf9e72fe0d90266982987d4a9109e1ad
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2022-12-16 09:59:40 +0100

    same-origin added

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -2,7 +2,9 @@
 
 describe("Test contact us page via webdriveruniversity", () => {
   it("should be able to submit a successfull submition via sub/form", () => {
-    cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    //cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.visit("https://www.webdriveruniversity.com");
+    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
     cy.document().should("have.property", "charset").and("eq", "UTF-8");
     cy.title().should("include", "WebDriver");
     cy.url().should("include", "contact");
@@ -16,7 +18,9 @@ describe("Test contact us page via webdriveruniversity", () => {
     cy.get("h1").should("have.text", "Thank You for your Message!");
   });
   it("should not be able to submit a successfull submition via sub/form ass all field are required", () => {
-    cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    //cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.visit("https://www.webdriveruniversity.com");
+    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
     //cy.get("#contact-us").click({ force: true });
     cy.get('[name="first_name"]').type("Joe");
     cy.get('[name="last_name"]').type("Shmoe");
