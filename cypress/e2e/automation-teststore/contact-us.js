dee323ad5fd8b9bef3445b8306a738af822f6833
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2022-12-25 15:21:53 +0100

    gitignore added

--- cypress/e2e/automation-teststore/contact-us.js
+++ cypress/e2e/automation-teststore/contact-us.js
@@ -6,24 +6,33 @@ describe("Test contact us page via automation store", () => {
   before(() => {
     cy.fixture("user-datails").as("user");
   });
-  it("should be able to submit a successfull submition via contact us form", () => {
-    cy.visit("https://automationteststore.com/");
-    cy.get("a[href$='contact']")
-      .click()
-      .then(function (contactText) {
-        console.log(`Selected the following item ${contactText.text()}`);
+  it(
+    "should be able to submit a successfull submition via contact us form",
+    {
+      retries: {
+        runMode: 0,
+        openMode: 2
+      }
+    },
+    () => {
+      cy.visit("https://automationteststore.com/");
+      cy.get("a[href$='contact']")
+        .click()
+        .then(function (contactText) {
+          console.log(`Selected the following item ${contactText.text()}`);
+        });
+      // cy.xpath("//a[contains(@href, 'contact')]").click();
+      cy.get("@user").then((user) => {
+        cy.get("#ContactUsFrm_first_name").type(user.first_name);
+        cy.get("#ContactUsFrm_email").type(user.email);
+        cy.get("#ContactUsFrm_enquiry").type(user.message);
       });
-    // cy.xpath("//a[contains(@href, 'contact')]").click();
-    cy.get("@user").then((user) => {
-      cy.get("#ContactUsFrm_first_name").type(user.first_name);
-      cy.get("#ContactUsFrm_email").type(user.email);
-      cy.get("#ContactUsFrm_enquiry").type(user.message);
-    });
 
-    cy.get("button[title='Submit']").click();
-    cy.get(".mb40 > :nth-child(3)").should(
-      "have.text",
-      "Your enquiry has been successfully sent to the store owner!"
-    );
-  });
+      cy.get("button[title='Submit']").click();
+      cy.get(".mb40 > :nth-child(3)").should(
+        "have.text",
+        "Your enquiry has been successfully sent to the store owner!"
+      );
+    }
+  );
 });
