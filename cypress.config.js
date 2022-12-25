474471b4ceebbd40a71ca94629b62c423f95a631
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2022-12-25 14:21:25 +0100

    config file updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,26 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs-extra");
+const path = require("path");
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve("cypress/config", `${file}.json`);
+
+  if (!fs.existsSync(pathToConfigFile)) {
+    console.log("No custom config file found.");
+    return {};
+  }
+
+  return fs.readJson(pathToConfigFile);
+}
 
 module.exports = defineConfig({
   projectId: "yvobr4",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const file = config.env.configFile || "";
+
+      return getConfigurationByFile(file);
     },
     specPattern: "cypress/e2e/**/*.{js, jsx, ts, tsx, feature}",
     excludeSpecPattern: "cypress/e2e/other/*.js",
@@ -17,6 +33,14 @@ module.exports = defineConfig({
     videoUploadOnPasses: false,
     viewportHeight: 1080,
     viewportWidth: 1920,
+    retries: {
+      runMode: 0,
+      openMode: 1
+    },
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      configFile: "reporter-config.json"
+    },
     defaultCommandTimeout: 10000,
     pageLoadTimeout: 120000,
     env: {
